79fe911206893ac6d8f88f2f86d1b76c670e19c2
Raihan <raihanhn20@gmail.com>
2023-09-13 14:34:00 +0600

    correction

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -55,7 +55,7 @@ export default function LoginForm() {
             </div>
           )}
           <Link className=" text-sm mt-3 text-right " href={"/register"}>
-            Don't have an account? <span className="underline">Register</span>{" "}
+            Don't have an account? <span className="underline">Register</span>
           </Link>
         </form>
       </div>
